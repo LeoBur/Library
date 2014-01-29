a3f1e89ec552459a45ee92dd3989e0bdb058dc51
Leandro Burgos <Leandro.Burgos@wnco.com>
2014-01-29 12:36:25 -0600

    Cambios en BookController para el mapping

--- src/main/java/war/webapp/controller/BookController.java
+++ src/main/java/war/webapp/controller/BookController.java
@@ -1,12 +1,21 @@
 package war.webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/books*")
 public class BookController {
+
     @Autowired
     BookManager bookManager;
+
+    /*@RequestMapping(method = RequestMethod.GET)
+    public ModelAndView handleRequest() throws Exception{
+        return new ModelAndView().addObject(bookManager.getBooks());
+    }*/
 
     @RequestMapping("/books*")
     public String execute(ModelMap model) {
